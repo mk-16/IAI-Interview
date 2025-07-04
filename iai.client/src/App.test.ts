f96012b73b362324dfc8162b099fe11aa0d88976
mk-16 <michael.laloush@gmail.com>
2025-07-05 02:31:36 +0300

    [Removed]: wrong script [Add]: Test Mock

---
+++ iai.client/src/App.test.ts
@@ -0,0 +1,13 @@
+
+it("is working", () => {
+    expect(true).toBe(true);
+})
+
+//it('works', async () => {
+
+//    //render(<Cesium />);
+//    await screen.findByRole('cesium-viewer').then((viewer) => {
+//        expect(viewer).toBe(typeof HTMLCanvasElement)
+//    })
+
+//})
